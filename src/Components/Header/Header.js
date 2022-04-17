cc78b287dcfac480c901e164ee7c67c022a44334
Sajib Hossain <sajib.sj359@gmail.com>
2022-04-17 14:10:05 +0600

    added nav bar

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -4,12 +4,20 @@ import { Link } from 'react-router-dom';
 const Header = () => {
     return (
         <div>
-            <nav>
+            <nav className='bg-slate-400  '>
+                <div className='text-3xl p-4 mx-auto flex justify-center gap-5'>
+                <div className='flex text-orange-800 font-thin'> 
+                    <img className='w-10 h-10' src="wedding logo.png" alt="" />
+                    <h3>Wedding Photographer</h3>
+                </div>
+           
+                
                 <Link to="/">Home</Link>
                 <Link to="/services">Services</Link>
-                <Link to = "/blogs">Blogs</Link>
+                <Link  to = "/blogs">Blogs</Link>
                 <Link to="/about">About</Link>
-                <Link to="/login">Login</Link>
+                <Link  to="/login">Login</Link>
+                </div>
             </nav>
         </div>
     );
