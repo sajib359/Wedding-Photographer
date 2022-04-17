b49cd40404cf5f39854953d1c4a66bb5b553f313
Sajib Hossain <sajib.sj359@gmail.com>
2022-04-17 13:28:56 +0600

    added all router and setup tailwind

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
-import logo from './logo.svg';
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import About from './Components/About/About';
+import Blogs from './Components/Blogs/Blogs';
+import Header from './Components/Header/Header';
+import Home from './Components/Home/Home';
+import Login from './Components/Login/Login';
+import NotFound from './Components/NotFound/NotFound';
+import Services from './Components/Services/Services';
 
 function App() {
   return (
     <div className="App">
-      
+        <Header></Header>
+      <Routes>
+        <Route path='/' element={<Home></Home>}></Route>
+        <Route path='/services' element={<Services></Services>}></Route>
+        <Route path='/blogs' element={<Blogs></Blogs>}></Route>
+        <Route path='/about' element={<About></About>}></Route>
+        <Route path='/login' element={<Login></Login>}></Route>
+        <Route path='/*' element={<NotFound></NotFound>}></Route>
+
+      </Routes>
+   
     </div>
   );
 }
---
+++ src/Components/Header/Header.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Header = () => {
+    return (
+        <div>
+            <nav>
+                <Link to="/">Home</Link>
+                <Link to="/services">Services</Link>
+                <Link to = "/blogs">Blogs</Link>
+                <Link to="/about">About</Link>
+                <Link to="/login">Login</Link>
+            </nav>
+        </div>
+    );
+};
+
+export default Header;
