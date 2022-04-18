a2cfce92961cffb3a4aa89ab16fb5a282ae337eb
Sajib Hossain <sajib.sj359@gmail.com>
2022-04-18 10:20:30 +0600

    Not found page added

--- src/Components/NotFound/NotFound.js
+++ src/Components/NotFound/NotFound.js
@@ -3,7 +3,9 @@ import React from 'react';
 const NotFound = () => {
     return (
         <div>
-            <h1>Searching Page Not Found <span>404</span></h1>
+            <h1 className='text-5xl font-bold mt-40'>Searching Page Not Found <span className='text-lime-400'>404</span></h1>
+
+            <img className='mx-auto' src="https://m.media-amazon.com/images/I/51GMrV8lp-L._UY695_.jpg" alt="" />
         </div>
     );
 };
